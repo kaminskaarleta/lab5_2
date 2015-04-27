34104886ed1030d6b5e2cc3bffbe5195ffd6d72e
Kaminska <180195@edu.p.lodz.pl>
2015-04-27 11:39:26 +0200

    Dodanie testów CRUD

--- src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
+++ src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
@@ -7,9 +7,12 @@ import java.util.List;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
 import edu.iis.mto.integrationtest.model.Person;
 
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 
 public class PersonRepositoryIntegrationTest extends IntegrationTest {
 
@@ -30,6 +33,23 @@ public class PersonRepositoryIntegrationTest extends IntegrationTest {
 		assertEquals(count + 1, personRepository.count());
 		assertEquals("Mancini", personRepository.findOne(count + 1)
 				.getLastName());
+	}
+	
+	@Test
+	public void testDeletePersonWithId1_countShoudBe1_idShouldBeNull() {
+		long count = personRepository.count();
+		personRepository.delete(a(person().withId(1)
+				.withFirstName("Piotr").withLastName("Morowiecki")));
+		assertEquals(null, personRepository.findOne(1L));
+		assertEquals(count - 1, personRepository.count());
+	}
+	
+	@Test
+	public void testUpdatePersonWithId1_CheckChange() {
+		personRepository.saveAndFlush(a(person().withId(1)
+				.withFirstName("Arleta").withLastName("Kaminska")));
+		assertEquals("Arleta", personRepository.findOne(1L).getFirstName());
+		assertEquals("Kaminska", personRepository.findOne(1L).getLastName());
 	}
 
 	private Person a(PersonBuilder builder) {
